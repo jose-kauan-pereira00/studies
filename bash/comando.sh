016d0b00acbcf5c2f37c66fb85d109dcc3e9f72b
Jose Kauan Pereira <jose.kauan.pereira00@gmail.com>
2026-05-02 12:01:41 -0300

    Created: Logica

---
+++ bash/comando.sh
@@ -0,0 +1,11 @@
+mkdir ola/
+cd ola/
+touch ola.txt
+echo "Olá, estou testando o uso de de sh. para criação de arquivos" > ola.txt
+echo "outra linha, so de teste." >> ola.txt
+cat ola.txt
+
+touch ola.py
+echo 'print("Hello, world!")' > ola.py
+python3 ola.py
+
